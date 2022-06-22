35a93fb0f3676b4106849b9c79306bcbc60babbe
bussudo <105820926+bussudo@users.noreply.github.com>
2022-06-21 22:55:38 -0400

    updated name

---
+++ Assets/js/script.js
@@ -0,0 +1,40 @@
+// Selects element by class
+var timeEl = document.querySelector(".time");
+
+// Selects element by id
+var mainEl = document.getElementById("main");
+
+var secondsLeft = 30;
+
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      // Calls function to create and append image
+      sendMessage();
+    }
+
+  }, 1000);
+}
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();
+
+$(function() {  
+  $("#button").click(function() { 
+       $("#button").addClass("onclic", 250, validate); 
+       });
+      })
