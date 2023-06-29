7385eee83baf7e0bdbc79af1de5218693b3c55ca
bussudo <105820926+bussudo@users.noreply.github.com>
2023-06-28 21:29:15 -0400

    functionality working with restart button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,28 +7,30 @@ var timerEl = document.querySelector("#timer");
 var startBtn = document.querySelector("#start");
 var startPage = document.querySelector(".start-page");
 var quizPage = document.querySelector("#quiz");
-var scorePage = document.querySelector("#score");
+var scorePage = document.querySelector("#scorePage");
+var lastSaveSection = document.querySelector("#lastPage");
 var quizQuestion = document.querySelector("#question");
 var quizAnswers = document.querySelector("#answers");
 var quizCorrect = document.querySelector("#ansRight");
 var quizEndEl = document.querySelector("#quizEnd");
 var submitBtn = document.querySelector("#submit");
+var restartBtn = document.querySelector("#restart");
+var listHighScores = [];
 
 var questionsEl = document.querySelector("#question");
 var answersEl = document.querySelector("#answers");
 var initialsEl = document.querySelector("#initials");
-
 var responseEl = document.querySelector("#response");
 
 var timerInterval;
 var index = 1;
 var counter = 60;
 var score = 0;
-var addInitials;
 startPage.style.display = "flex";
 quizPage.style.display = "none";
 scorePage.style.display = "none";
-
+lastPage.style.display = "none";
+console.log(lastPage);
 var startCountdown;
 // WHEN I click the start button
 window.onload = function () {
@@ -43,36 +45,27 @@ window.onload = function () {
       let timeTag = "<span>Time Left: " + counter + "</span>";
       timeRem.innerHTML = timeTag;
     }
-
     startPage.style.display = "none";
     quizPage.style.display = "flex";
     scorePage.style.display = "none";
-
-    let timeRem = document.querySelector("#timer");
-    let timeTag = "<span>Time Left: " + counter + "</span>";
-    timeRem.innerHTML = timeTag;
-
+    lastPage.style.display = "none";
     startCountdown = setInterval(countdown, 1000);
-
     getQuestion(question, curQuestIndex);
   };
 };
 
 function getQuestion() {
   var curQuest = questions[curQuestIndex];
-
   var questionEl = document.getElementById("question");
   questionEl.textContent = curQuest.question;
-
   // clears out old answers
   answersEl.innerHTML = "";
-
   curQuest.answers.forEach(function (answer, i) {
     i++;
     var answerNode = document.createElement("button");
     answerNode.setAttribute("class", "answer");
     answerNode.setAttribute("value", answer);
-    answerNode.textContent = i + ". " + answer;
+    answerNode.textContent = i + 1 + ". " + answer;
     //attach click event listener to each choice
     answerNode.onclick = questionClick;
     //display question
@@ -84,39 +77,38 @@ function getQuestion() {
 function questionClick() {
   if (this.value !== questions[curQuestIndex].ansRight) {
     counter -= 10;
-    console.log(timer);
     responseEl.textContent =
       "Wrong!  The correct answer is " + questions[curQuestIndex].ansRight;
-  } else if (this.value == questions[curQuestIndex].ansRight) {
+  } else {
     responseEl.textContent = "Correct!";
     //add correct answer to score
     score = score + 5;
     console.log(score);
   }
+
   responseEl.setAttribute("class", "response");
   setTimeout(function () {
     responseEl.setAttribute("class", "response hide");
+    checkforNext();
   }, 1000);
-
+}
+function checkforNext() {
+  console.log("checking questions length");
   curQuestIndex++;
 
-  if (curQuestIndex === questions.length) {
+  if (curQuestIndex >= questions.length) {
+    //  if (curQuestIndex === questions.length) {
     clearInterval(startCountdown);
+    console.log(curQuestIndex);
     quizEnd();
   } else {
     getQuestion();
   }
 }
-
 //end quiz
 function quizEnd() {
   clearInterval(timerId);
-
-  let lastPafeSection = document.querySelector(".lastpage");
-  lastPafeSection.style.display = "block";
-  let timeRem = document.querySelector("#seconds-left");
-  let timeTag = "<span>Time Left: " + counter + "</span>";
-  timeRem.innerHTML = timeTag;
+  lastPage.style.display = "block";
   startPage.style.display = "none";
   quizPage.style.display = "none";
   scorePage.style.display = "flex";
@@ -131,17 +123,21 @@ submitBtn.addEventListener("click", saveScore);
 
 function saveScore() {
   console.log("clicked to save the high scores");
+  var initialsEl = document.getElementById("initials");
+  // initialsEl.textContent = curScore.initials;
+  initialsEl.textContent = initials;
+  // document.getElementById('textbox_id').value
   var initials = initialsEl.value.trim();
   if (initials !== "") {
-    // var curScore = JSON.parse(window.localStorage.getItem("curScore")) || [];
-    score = JSON.parse(window.localStorage.getItem("score")) || [];
-
     console.log(score);
-
-    // curScore.push(curScore);
-    // window.localStorage.setItem("curScore", JSON.stringify(curScore));
-    score.push(score);
-    window.localStorage.setItem("score", JSON.stringify(score));
+    console.log(initials);
+    var curScore = {
+      score: score,
+      initials: initials,
+    };
+    listHighScores.push(curScore);
+    localStorage.setItem("list", JSON.stringify(listHighScores));
+    console.log(curScore);
   }
 }
 
@@ -152,11 +148,33 @@ function checkForEnter(event) {
   }
 }
 
-var scoreEl = document.querySelector("score");
+// function nextQuestion() {
+//   queCount++;
+//   console.log(queCount);
+//     if (queCount == queCount) {
+//       quizEnd();
+//     console.log("quizend2");
+//   }
+// }
 
-function nextQuestion() {
-  queCount++;
-  if (queCount == queCount) {
-    quizEnd();
-  }
+function restart() {
+  startPage.style.display = "flex";
+  quizPage.style.display = "none";
+  scorePage.style.display = "none";
+  lastPage.style.display = "none";
+  document.getElementById("score").innerHTML = "";
+  timerEl.innerHTML = "";
+  counter = 60;
+  queCount = 0;
+  curQuestIndex = 0;
+  score = 0;
+  timer = "";
+
+  curScore = "";
 }
+
+submitBtn.onclick = saveScore;
+
+restartBtn.onclick = restart;
+
+initialsEl.onkeyup = checkForEnter;
